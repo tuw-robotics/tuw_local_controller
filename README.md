85246e016eb16bcbba4300d2047a83ed055411f1
Axel Brunnbauer <axel.brunnbauer@gmx.at>
2019-08-21 15:06:51 +0200

    documentation

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# tuw_local_controller
+# Overview
+This package defines ROS message interfaces which have to be implemented by
+local controllers (LC) in order to be used by behaviour controllers.
+
+The communication between a client and a LC is established
+via the action mechanism of ROS. The client sends a goal, which contains
+the path to be executed, to a LC. The LC then executes the path and reports
+back feedback messages to the client containing the current processing step
+on the path. If a LC finished the path, it sends a message containing the
+robot's current location back to the client.
+
+# Architecture
+
+## Package descriptions
+
+- **tuw_local_controller_msgs:** This package contains the action and message definition file.
+- **tuw_local_controller_util:** Contains interfaces for clients and LC which already handle the network
+communication between them. Users of the interfaces only have to implement the callbacks
+in order to process/send requests.
+# Available implementations
+- [tuw_multi_robot_ctrl](https://github.com/tuw-robotics/tuw_multi_robot.git): 
+The tuw_multi_robot package contains a simple local controller implementing the action interface.
--- tuw_local_controller_util/include/tuw_local_controller_util/LocalControllerClient.h
+++ tuw_local_controller_util/include/tuw_local_controller_util/LocalControllerClient.h
@@ -5,16 +5,42 @@
 #include <tuw_local_controller_msgs/ExecutePathAction.h>
 
 namespace tuw_local_controller::util {
+
+    //* Utility client für communicating with local controller actions.
+    /**
+     * Establishes and handles the communication with a local controller implementing the ExecutePathAction pattern.
+     */
     class LocalControllerClient {
     public:
+
+        /*!
+         * Initializes the communication between client and action server. 
+         * \param node_handle A valid ROS node handle.
+         * \param name The topic name, under which the local controller operates.
+         */
         LocalControllerClient(ros::NodeHandle &node_handle, const std::string &name = "local_controller");
 
+        /*!
+         * \brief Sends a goal to the specified topic of a local controller.
+         * \param goal A goal message which will be executed by the local controller.
+         */
         void sendGoal(const tuw_local_controller_msgs::ExecutePathGoal &goal);
 
+        /*!
+         * \brief Cancels the goal which is executed currently. If no goal is currently active, nothing happens.
+         */
         void cancelCurrentGoal();
 
+        /*!
+         * \brief Handles the feedback which was sent by the local controller while executing the path.
+         * \param feedback A feedback message containing the current progress.
+         */
         virtual void onFeedbackReceived(const tuw_local_controller_msgs::ExecutePathFeedback &feedback) = 0;
 
+        /*!
+         * \brief Handles the result message sent by the local controller when it has successfully completed the path.
+         * \param result The result upon termination.
+         */
         virtual void onResultReceived(const tuw_local_controller_msgs::ExecutePathResult &result) = 0;
 
     private:
